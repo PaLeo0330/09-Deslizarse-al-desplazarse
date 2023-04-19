c69c365f29d1c024429caa1ff0c20612fd0afd2f
Leonardo Guerra <leonardoguerra1994hhotmail.com>
2023-04-19 12:16:28 -0500

    Project completed

--- script.js
+++ script.js
@@ -1,34 +1,45 @@
 function debounce(func, wait = 20, immediate = true) {
-    var timeout;
-    return function() {
-      var context = this, args = arguments;
-      var later = function() {
-        timeout = null;
-        if (!immediate) func.apply(context, args);
-      };
-      var callNow = immediate && !timeout;
-      clearTimeout(timeout);
-      timeout = setTimeout(later, wait);
-      if (callNow) func.apply(context, args);
+  var timeout;
+  return function () {
+    var context = this,
+      args = arguments;
+    var later = function () {
+      timeout = null;
+      if (!immediate) func.apply(context, args);
     };
+    var callNow = immediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if (callNow) func.apply(context, args);
   };
+}
 
-  const sliderImages = document.querySelectorAll('.slide-in');
+const sliderImages = document.querySelectorAll(".slide-in");
 
-  function checkSlide() {
-    sliderImages.forEach(sliderImage => {
-     
-      const slideInAt = (window.scrollY + window.innerHeight) - sliderImage.height / 2;
-     
-      const imageBottom = sliderImage.offsetTop + sliderImage.height;
-      const isHalfShown = slideInAt > sliderImage.offsetTop;
-      const isNotScrolledPast = window.scrollY < imageBottom;
-      if (isHalfShown && isNotScrolledPast) {
-        sliderImage.classList.add('active');
-      } else {
-        sliderImage.classList.remove('active');
-      }
-    });
+function checkSlide() {
+  sliderImages.forEach((sliderImage) => {
+    const slideInAt =
+      window.scrollY + window.innerHeight - sliderImage.height / 2;
+
+    const imageBottom = sliderImage.offsetTop + sliderImage.height;
+    const isHalfShown = slideInAt > sliderImage.offsetTop;
+    const isNotScrolledPast = window.scrollY < imageBottom;
+    if (isHalfShown && isNotScrolledPast) {
+      sliderImage.classList.add("active");
+    } else {
+      sliderImage.classList.remove("active");
+    }
+  });
+}
+
+window.addEventListener("scroll", debounce(checkSlide));
+
+// function to sum all numbers in an array
+function sumAll(arr) {
+  let sum = 0;
+  for (let i = Math.min(...arr); i <= Math.max(...arr); i++) {
+    sum += i;
   }
+  return sum;
+}
 
-  window.addEventListener('scroll', debounce(checkSlide));
